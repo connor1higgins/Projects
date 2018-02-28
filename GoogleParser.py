beb1148f511ca3874ed055b5b3e4696818af89d7
connor1higgins <33924445+connor1higgins@users.noreply.github.com>
2018-02-28 09:46:33 -0800

    Add files via upload

---
+++ GoogleParser.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 08:44:47 2018
+
+@author: Connor Higgins
+"""
+
+from googlesearch import search
+import time
+def GoogleParser(kw, numberof):
+    time.sleep(2) # to prevent annoying lord google
+    urls = []
+    for url in search(str(kw), stop=int(numberof)):
+        urls.append(url)
+    return(urls)
